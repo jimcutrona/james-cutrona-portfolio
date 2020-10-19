dddf8e8e90852e882537dc7b2feec8b0b1822ede
James Cutrona <jamescutrona@gmail.com>
2020-10-19 01:12:06 -0700

    second commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-
-// wait for DOM to load first
-document.addEventListener('DOMContentLoaded', function () {
-  window.onscroll = function () { sticky() };
-  //target scroll points using the top of selected elements 
-  let stick = document.getElementById("navbar").offsetTop;
-  let stickAgain = document.querySelector("main").offsetTop;
-  //stick to top of screen when nav's absolute position is scrolled to 
-  //stick to welcome section when user scrolls to the bottom of that element
-  function sticky() {
-    if (window.pageYOffset > stick) {
-      navbar.classList.add("stick");
-      navbar.classList.remove("nav")
-    } else if (window.pageYOffset < stickAgain) {
-      navbar.classList.remove("sticky");
-      navbar.classList.add("nav")
-    }
-  }
+
+// wait for DOM to load first
+document.addEventListener('DOMContentLoaded', function () {
+  window.onscroll = function () { sticky() };
+  //target scroll points using the top of selected elements 
+  let stick = document.getElementById("navbar").offsetTop;
+  let stickAgain = document.querySelector("main").offsetTop;
+  //stick to top of screen when nav's absolute position is scrolled to 
+  //stick to welcome section when user scrolls to the bottom of that element
+  function sticky() {
+    if (window.pageYOffset > stick) {
+      navbar.classList.add("stick");
+      navbar.classList.remove("nav")
+    } else if (window.pageYOffset < stickAgain) {
+      navbar.classList.remove("sticky");
+      navbar.classList.add("nav")
+    }
+  }
 })
